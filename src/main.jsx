ae22ebe3c90e65863389216fcaaf1009fb45de2d
Sundar Karthik S <sundar1007karthik@gmail.com>
2025-09-03 18:01:42 +0530

    Add ErrorButton component for testing Sentry error tracking

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,37 @@ import { UserProvider } from './context/UserContext.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Sentry imports
+import * as Sentry from "@sentry/react";
+
+
+// Initialize Sentry
+Sentry.init({
+  dsn: "https://5a187cbd8662dba3061a3bfd5c61de69@o4509518894268416.ingest.de.sentry.io/4509955623551056",
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/infoziant-security\.sentry\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  enableLogs: true,
+});
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-        <ToastContainer position="top-right" autoClose={3000} />
-      </UserProvider>
-    </BrowserRouter>
+    <Sentry.ErrorBoundary fallback={<p>Something went wrong.</p>}>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+          <ToastContainer position="top-right" autoClose={3000} />
+        </UserProvider>
+      </BrowserRouter>
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
